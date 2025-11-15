6631521317739fc80c24533a718599adb1689a7f
threecatswink <minecraftmaster9900@gmail.com>
2025-11-15 16:41:52 -0600

    ANOTHER HOTFIX
    just covering all of the bases....

--- src/PDFviewer.tsx
+++ src/PDFviewer.tsx
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from "react";
 
 import * as pdfjsLib from "pdfjs-dist";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url";
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}${pdfjsWorker}`;
 
 import Toolbar from "./Toolbar";
 
