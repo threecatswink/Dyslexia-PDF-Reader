f3817d33d6df3735386b65137b42ad6fdf4dd0a7
threecatswink <minecraftmaster9900@gmail.com>
2025-11-15 16:55:09 -0600

    more small editing

--- src/PDFviewer.tsx
+++ src/PDFviewer.tsx
@@ -2,7 +2,7 @@ import { useRef, useState, useEffect } from "react";
 
 import * as pdfjsLib from "pdfjs-dist";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url";
-pdfjsLib.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}${pdfjsWorker}`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = import.meta.env.BASE_URL + '/Dyslexia-PDF-Reader/' + pdfjsWorker;
 
 import Toolbar from "./Toolbar";
 
