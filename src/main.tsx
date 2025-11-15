4ecb494d1838567c4683578f9f99a2f62339f50b
threecatswink <minecraftmaster9900@gmail.com>
2025-11-15 17:04:49 -0600

    removing changes
    oops

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PDFviewer from './PDFviewer.tsx'
-import * as pdfjsLib from "pdfjs-dist";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url";
-
-pdfjsLib.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}${pdfjsWorker}`;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
