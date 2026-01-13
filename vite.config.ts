72b0f0b6df51279420ad7a769a5d3eee6c804f2f
Andrew Winkelman <153324029+threecatswink@users.noreply.github.com>
2026-01-12 23:09:35 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/Dyslexia-PDF-Reader/',
   plugins: [
     react(),
     tailwindcss(),
